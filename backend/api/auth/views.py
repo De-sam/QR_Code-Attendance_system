e07c3b86b76fce0992ede8b8a5d7418aad262287
Adams Adeyiga <adamsadeyiga1@gmail.com>
2024-03-19 15:13:50 +0100

    ADD - User Model

--- backend/api/auth/views.py
+++ backend/api/auth/views.py
@@ -1,16 +1,50 @@
-from flask_restx import Namespace, Resource
+from flask import request
+from flask_restx import Namespace, Resource, fields
+from ..models.users import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from http import HTTPStatus
 
 auth_namespace = Namespace('auth', description="Namespace for authentication")
+signup_schema = auth_namespace.model(
+    'SignUp', {
+        'id': fields.Integer(description="User Identification Number"),
+        'username': fields.String(required=True, description="Username"),
+        'email': fields.String(required=True, description="User Email"),
+        'password': fields.String(required=True, description="User Password"),
+
+    }
+)
+
+user_schema = auth_namespace.model(
+    'User', {
+        'id': fields.Integer(description="User Identification Number"),
+        'username': fields.String(required=True, description="Username"),
+        'email': fields.String(required=True, description="User Email"),
+        'password_hash': fields.String(required=True, description="User Password"),
+        'is_active': fields.Boolean(decription="This shows that User is active"),
+        'is_staff': fields.Boolean(decription="This shows if User is a staff")
+    }
+)
 
 
 @auth_namespace.route('/signup')
 class SignUp(Resource):
 
+    @auth_namespace.expect(signup_schema)
+    @auth_namespace.marshal_with(user_schema)
     def post(self):
         """
         Create new user
         """
-        pass
+        data = request.get_json()
+        new_user = User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password_hash=generate_password_hash(data.get('password'))
+        )
+
+        new_user.save()
+        return new_user, HTTPStatus.CREATED
 
 
 @auth_namespace.route('/login')
--- backend/api/models/users.py
+++ backend/api/models/users.py
@@ -10,9 +10,13 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password_hash = db.Column(db.Text(), nullable=False)
     is_staff = db.Column(db.Boolean(), default=False)
-    is_active = db.Column(db.Boolean(), default=False)
+    is_active = db.Column(db.Boolean(), default=True)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow())
     updated_at = db.Column(db.DateTime(), default=datetime.utcnow())
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
