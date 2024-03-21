2e19f1cd0825db6256ac071de810e6029ff2597b
sam <samsoncoded@gmail.com>
2024-03-21 16:02:13 +0100

    Init fronend design

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+	return (
+		<div className='login'>
+			<input type="text" />
+			<input type="text" />
+
+		</div>
+	)
+}
+
+export default Login
