09b06fccb5fa8b42b964850afe53c4a65e44c451
De-sam <samsoncoded@gmail.com>
2024-03-23 22:04:27 +0100

    updated admin,attendance, clocking events and locations table

--- backend/api/models/location.py
+++ backend/api/models/location.py
@@ -8,14 +8,14 @@ class Location(db.Model):
     LocationIdID = db.Column(db.Integer(), primary_key=True)
     Name= db.Column(db.String(45), nullable=False, unique=True)
     Address= db.Column(db.String(50), nullable=False, unique=True)
-    Long= db.Column(db.Text(), nullable=False)
-    Lat= db.Column(db.Boolean(), default=False)
-    Alias= db.Column(db.Boolean(), default=True)
+    Long= db.Column(db.float(), nullable=False)
+    Lat= db.Column(db.float(), default=False)
+    Alias= db.Column(db.string(), default=True)
     CreatedAt = db.Column(db.DateTime(), default=datetime.utcnow())
     UpdatedAt = db.Column(db.DateTime(), default=datetime.utcnow())
 
     def __repr__(self):
-     return f"<User username='{self.Username}', email='{self.Email}', active='{self.IsActive}'>"
+     return f"<company alias='{self.Alias}', address='{self.Address}', name='{self.Name}'>"
 
 
     def save(self):
