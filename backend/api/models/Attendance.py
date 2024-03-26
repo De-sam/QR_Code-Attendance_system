6b1162dd65610e005ec168c531c35259b9b77076
De-sam <samsoncoded@gmail.com>
2024-03-26 15:10:07 +0100

    associating company table with user table

--- backend/api/models/Attendance.py
+++ backend/api/models/Attendance.py
@@ -6,13 +6,13 @@ from users import User
 class AttendanceReport(db.Model):
     __tablename__ = 'attendancereport'
     ReportID = db.Column(db.Integer, primary_key=True)
-    UserID = db.Column(db.Integer, db.ForeignKey('users.UserID'))
+    UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'))
     Date = db.Column(db.Date)
     ClockInTime = db.Column(db.Time)
     ClockOutTime = db.Column(db.Time)
     CreatedAt = db.Column(db.DateTime, default=datetime.utcnow)
     UpdatedAt = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    UserID = db.column(db.Integer, db.ForeignKey('user.UserID')
+
 
     def __repr__(self):
         return f"<AttendanceReport Date='{self.Date}', Username='{self.user.username}', TimeIn='{self.ClockInTime}', TimeOut='{self.ClockOutTime}'>"
